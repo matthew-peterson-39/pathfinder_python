50ddc032348f458a11f620b99891aa72c7849c20
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-07 18:55:16 -0500

    requirements.txt added.

---
+++ astar.py
@@ -0,0 +1,13 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+pygame.init()
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+CLOCK = pygame.time.Clock()
+running = True
+
+while
+
+pygame.display.set_caption("A* Path Finding Algorithm")
