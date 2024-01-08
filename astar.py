9472a359f9183332ddb7772966099c933c34a5a8
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2024-01-07 19:48:30 -0500

    color codes added

--- astar.py
+++ astar.py
@@ -2,12 +2,17 @@ import pygame
 import math
 from queue import PriorityQueue
 
-pygame.init()
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-CLOCK = pygame.time.Clock()
-running = True
-
-while
-
 pygame.display.set_caption("A* Path Finding Algorithm")
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+ORANGE = (255,165,0)
+GREY = (128,128,128)
+TURQUOISE = (64,224,208)
+PURPLE = (128,0,128)
