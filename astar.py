da81815b44b4900e0346425c6f1f54868cfce2da
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2024-01-07 19:52:39 -0500

    Node class init created

--- astar.py
+++ astar.py
@@ -16,3 +16,18 @@ ORANGE = (255,165,0)
 GREY = (128,128,128)
 TURQUOISE = (64,224,208)
 PURPLE = (128,0,128)
+
+class Node:
+    #location of node
+    #width of node
+    #color of node
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
+        
