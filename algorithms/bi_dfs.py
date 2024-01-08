68e36f2d4d9cf9b306e1da135af68b1bd8e664c6
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2024-01-08 14:38:15 -0500

    added additional end_stack and logic for bi_dfs.

--- algorithms/bi_dfs.py
+++ algorithms/bi_dfs.py
@@ -1,36 +1,41 @@
 def bidirectional_dfs(draw, start, end):
-    """
-    Implements a bidirectional Depth First Search (Bi-DFS) to find a path from a start node to an end node concurrently.
+    start_stack = [start]
+    start_visited = set()
+
+    end_stack = [end]
+    end_visited = set()
     
-    Params:
-    draw (function): Function to update the GUI.
-    start (Node): The starting point of the search.
-    end (Node): The target node to find.
 
-    Returns:
-        bool: True if a path to the end node is found, False if there's no path.
-    """
-   
-    stack = [start]
-    visited = set()
+    while start_stack and end_stack:
+        start_current = start_stack.pop()
+        start_visited.add(start_current)
 
-    while stack:
-        current = stack.pop()
-        visited.add(current)
+        end_current = end_stack.pop()
+        end_visited.add(end_current)
 
-        if current == end:
+        if start_current == end_current or \
+            start_current in end_stack or \
+            end_current in start_stack:
             #TODO visualize path
             return True
         
-        for neighbor in current.neighbors:
-            if neighbor not in visited:
-                stack.append(neighbor)
-                if neighbor != end:
+        for neighbor in start_current.neighbors:
+            if neighbor not in start_visited:
+                start_stack.append(neighbor)
+                if neighbor != end_current:
+                    neighbor.make_open()
+        
+        for neighbor in end_current.neighbors:
+            if neighbor not in end_visited:
+                end_stack.append(neighbor) 
+                if neighbor != start_current:
                     neighbor.make_open()
 
         draw()  # Redraw the grid with updated nodes
 
-        if current != start:
-            current.make_closed()  # Mark the current node as visited/closed in visualization
+        if start_current != start:
+            start_current.make_closed()
+        if start_current != start:
+            start_current.make_closed()
 
     return False
