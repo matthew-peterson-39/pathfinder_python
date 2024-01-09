40b0c9f8f1889884717ebe83b2a2b42c294130e0
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-09 18:38:03 -0500

    skeleton code added

--- sorting.py
+++ sorting.py
@@ -8,5 +8,18 @@ pygame.mouse.set_cursor(*pygame.cursors.arrow)
 pygame.display.set_caption("Sorting Visualizer")
 
 # generate a 50 element array of random values 1-100
-arr = [random.randint()]
+arr = [random.randint(1, 100) for _ in range(50)]
+
+# Draw array
+def draw_array():
+    pass
+
+def bubble_sort():
+    pass
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
