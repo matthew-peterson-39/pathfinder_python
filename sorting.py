7452ab2920fdbe2f2fb08f246eb9ca9650ec8988
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2024-01-10 16:10:32 -0500

    sorting color update added

--- sorting.py
+++ sorting.py
@@ -3,37 +3,57 @@ import random
 import Line
 
 pygame.init()
+
 WIDTH, HEIGHT = 800, 800
+GAP = 5
+LINE_WIDTH = 10
+NUM_LINES = 25
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
 pygame.mouse.set_cursor(*pygame.cursors.arrow)
 pygame.display.set_caption("Sorting Visualizer")
 
-# generate a 50 element array of random values 1-100
-arr = [random.randint(1, 100) for _ in range(50)]
+def create_lines(num_lines):
+    lines_arr = []
+    for i in range(num_lines):
+        random_height = random.randint(1,100)
+        height = (HEIGHT // 100) * random_height
+        x = i * (LINE_WIDTH + GAP)
+        y = HEIGHT - height
+        line = Line.Line(x,y, LINE_WIDTH, height, color=(120,120,0))
+        lines_arr.append(line)
+    return lines_arr
 
 # Draw array
-def draw_array():
-    width = 10
-    gap = 5
-    for i, j in enumerate(arr):
-        height = (HEIGHT / 100) * j
-        x = (width + gap) * i
-        y = HEIGHT - height 
-        line = Line.Line(x, y, width, height, color=(120,120,0))
-        line.draw(WIN)
+def draw_array(arr):
+    WIN.fill((0,0,0))  # Clear the screen
+    for line in arr:
+        line.draw(WIN)  # Draw each line
+    pygame.display.flip()  # Update the display
+
 
 def bubble_sort(arr):
+    swapped = True
     lines = len(arr)
-    for i in range(lines):
+    while swapped:
         swapped = False
-        for j in range(0, lines-i-1):
-            if arr[j] > arr[j+1]:
+        for i in range(0, lines-1):
+            arr[i].set_color((0,255,0))
+            arr[i+1].set_color((0,255,0))
+            
+            if arr[i].height > arr[i+1].height:
                 swapped = True
-    
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                pygame.time.delay(50)
+            draw_array(arr)
+
     if not swapped:
         pass
 
 
+unsorted_lines = create_lines(NUM_LINES)
 running = True
 while running:
     for event in pygame.event.get():
@@ -41,6 +61,7 @@ while running:
             running = False
     
     WIN.fill((0,0,0))
-    draw_array()
+    draw_array(unsorted_lines)
+    bubble_sort(unsorted_lines)
     pygame.display.flip()
 
