14643af4b5ad8d29f90401f9ac82f7d069a3bb22
matthew-peterson-39 <matthew.s.peterson.39@gmail.com>
2024-01-09 20:56:20 -0500

    Line visualization added

--- Line.py
+++ Line.py
@@ -4,15 +4,15 @@ class Line:
     def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
-        self.width = width
-        self.height =  height
+        self.width = width  
+        self.height =  height   # Uses the array values from sorting.py
         self.color = color
     
+    def set_color(self, new_color):
+        self.color = new_color
+
     def draw(self, window):
-        pass
+        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
 
     def update_height(self, new_height):
-        pass
-
-    def change_color(self, new_color):
         pass
--- sorting.py
+++ sorting.py
@@ -1,9 +1,10 @@
 import pygame
 import random
+import Line
 
 pygame.init()
-WIDTH = 800
-WIN = pygame.display.set_mode((WIDTH, WIDTH))
+WIDTH, HEIGHT = 800, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.mouse.set_cursor(*pygame.cursors.arrow)
 pygame.display.set_caption("Sorting Visualizer")
 
@@ -12,14 +13,36 @@ arr = [random.randint(1, 100) for _ in range(50)]
 
 # Draw array
 def draw_array():
-    pass
+    width = 10
+    gap = 5
+    for i, j in enumerate(arr):
+        height = (HEIGHT / 100) * j
+        x = (width + gap) * i
+        y = HEIGHT - height 
+        line = Line.Line(x, y, width, height, color=(120,120,0))
+        line.draw(WIN)
 
-def bubble_sort():
-    pass
+def bubble_sort(arr):
+    sorted = []
+    lines = len(arr)
+    for i in range(lines):
+        swapped = False
+        for j in range(0, lines-i-1):
+            if arr[j] > arr[j+1]:
+                swapped = True
+    
+    if not swapped:
+        pass
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    WIN.fill((0,0,0))
+    draw_array()
+    bubble_sort(arr)
+    pygame.display.flip()
 
